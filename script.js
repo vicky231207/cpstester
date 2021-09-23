b451119811f5fe9332a45b58907ba11179f4b4e7
vicky231207 <51056300+vicky231207@users.noreply.github.com>
2021-09-23 17:45:19 +0200

    Adding Files 🎊

---
+++ script.js
@@ -0,0 +1,56 @@
+const clicksEl = document.querySelector('#clicks');
+const timeEl = document.querySelector('#time');
+const cpsEl = document.querySelector('#cps');
+
+const clickarea = document.querySelector('#clickarea');
+
+let clicks = 0;
+let started = false;
+let time = 0;
+let maxTime = 1;
+
+const cpstimeEl = document.querySelector('#cpstime');
+const slider = document.querySelector('input');
+
+slider.oninput = () => {
+    cpstimeEl.innerText = slider.value + 's';
+    maxTime = slider.value;
+}
+
+
+const c = () => {
+    if ( !started ) {
+        document.querySelector('#cpstime-section').classList.add('hide');
+        clickarea.textContent = 'Click Here';
+        started = true;
+        setInterval ( timer, 10 );
+    }
+    
+    clicks++;
+    clicksEl.textContent = 'Clicks: ' + clicks;
+}
+
+clickarea.addEventListener('click', c);
+
+const timer = () => {
+    time++;
+    
+    if ( time / 100 >= maxTime ) {
+        clearInterval();
+
+        clickarea.removeEventListener('click', c);
+
+        time = 100;
+        const cps = Math.round(clicks / (time / 100) * 100) / 100;
+
+        localStorage.setItem('cps', cps);
+
+        window.location.replace("/cps");
+    }
+ 
+    timeEl.textContent = 'Time: ' + time / 100;
+
+    const cps = Math.round(clicks / (time / 100) * 100) / 100;
+    cpsEl.textContent = 'Cps: ' + cps;
+
+}
