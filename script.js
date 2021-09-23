3cd9784e190b112d56881d3d3558fe7e25575646
vicky231207 <51056300+vicky231207@users.noreply.github.com>
2021-09-23 20:06:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const timer = () => {
         clickarea.removeEventListener('click', c);
 
         time = timeMax * 100;
-        const cps = Math.round(clicks / maxTime);
+        const cps = Math.round(clicks / maxTime * 100) / 100;
 
         localStorage.setItem('cps', cps);
 
